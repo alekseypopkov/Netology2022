2dc56c8c162028106e4a04a3a36b6ea8ff8d5504
Alexey Popkov <alvlpv969@gmail.com>
2022-11-29 14:51:17 +1000

    create_db

--- Работа с PostgreSQL. Создание БД/Домашнее задание к лекции Работа с PostgreSQL из Python/main.py
+++ Работа с PostgreSQL. Создание БД/Домашнее задание к лекции Работа с PostgreSQL из Python/main.py
@@ -1,8 +1,30 @@
-
 import psycopg2
+import sys
 
 def create_db(conn):
-    pass
+    cur = conn.cursor()
+    cur.execute("""
+    DROP TABLE telephone;
+    DROP TABLE contacts;
+    """)
+
+    cur.execute("""
+            CREATE TABLE IF NOT EXISTS contacts(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(35) UNIQUE,
+                last_name VARCHAR(35) UNIQUE,
+                email VARCHAR(255) UNIQUE
+                );
+        """)
+    cur.execute("""
+            CREATE TABLE IF NOT EXISTS telephone(
+                id SERIAL PRIMARY KEY,
+                contactc_id INTEGER,
+                tel_number INTEGER
+                );
+    """)
+    conn.commit()  # фиксируем в БД
+    return print('\n Создание структуры БД (Таблиц) создано.\n Приступайте к наполнению БД.')
 
 def add_client(conn, first_name, last_name, email, phones=None):
     pass
@@ -23,7 +45,32 @@ def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     pass
 
 
-with psycopg2.connect(database="clients_db", user="postgres", password="postgres") as conn:
-    pass  # вызывайте функции здесь
+with psycopg2.connect(database="clients_db", user="postgres", password="Alex1869") as conn:
+    while True:
+        print(f'\nВведите c - чтобы создать структуру БД (таблицы) '
+            f'\nвведите s - чтобы узнать номер полки документа '
+            f'\nвведите l - чтобы вывести список всех документов '
+            f'\nвведите a - чтобы добавить новый документ '
+            f'\n* - завершить работу')
+        letter = input('Введите название команды: ')
+        if letter == 'c':
+            print(f'\nВнимание создание структуры БД приведет к уничтожению всех ранее введенных данных'
+                f'\nвведите p - чтобы продолжить'
+                f'\n* - вернуться в главное меню')
+            answer = input('Введите название команды: ')
+            if answer == 'p':
+                create_db(conn)
+            elif answer == '*':
+                main()
 
-conn.close()
+        #     name_people()
+        # elif letter == 's':
+        #     search_shelf()
+        # elif letter == 'l':
+        #     list_doc()
+        # elif letter == 'a':
+        #     add_doc()
+        elif letter == '*':
+            sys.exit()
+
+    conn.close()
